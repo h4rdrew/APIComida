59ab69ea5e4a09d1711d16d3fb0c95a528bf1d6f
Mateus Moraes <mtu_h@hotmail.com>
2021-08-13 17:26:58 -0300

    Resto da API

---
+++ APIComidaTeste/Controllers/AlimentoController.cs
@@ -0,0 +1,39 @@
+﻿using APIComidaTeste.DA.Views;
+using APIComidaTeste.Lib.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System;
+using System.Net;
+
+namespace APIComidaTeste.Controllers
+{
+    [ApiController]
+    [Route("alimentos")]
+    public class AlimentoController : Controller
+    {
+        private readonly IDatabase _db;
+        public AlimentoController(IDatabase db)
+        {
+            _db = db;
+        }
+
+
+
+        [HttpGet]
+        [Route("listar_alimentos")]
+        [SwaggerResponse((int)HttpStatusCode.OK, "", typeof(Alimento_SQLite))]
+        public IActionResult ConsultaAlimentos()
+        {
+            var resultado = _db.Alimento.ListarAlimentos();
+            return Ok();
+        }
+        [HttpPost]
+        [Route("cadastrar_alimento")]
+        [SwaggerResponse((int)HttpStatusCode.OK, "", typeof(Alimento_SQLite))]
+        public IActionResult CadastrarAlimento([FromForm] Lib.ModelsView.AlimentoParametros alimentoParametros)
+        {
+            return Ok();
+            //var resultado = _db.Alimento.CadastroAlimento(alimentoParametros);
+        }
+    }
+}
---
+++ APIComidaTeste/Startup.cs
@@ -0,0 +1,63 @@
+using APIComidaTeste.Lib.Interfaces;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+using System.IO;
+
+namespace APIComidaTeste
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var db = getDatabase_Sqlite();
+            db.Config();
+
+            services.AddSingleton(db);
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "APIComidaTeste", Version = "v1" });
+            });
+        }
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "APIComidaTeste v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+        private IDatabase getDatabase_Sqlite()
+        {
+            var dbPath = Path.Combine("Alimentos", "alimento.sqlitedb");
+            var db = new DA.Views.Database(dbPath);
+            return db;
+        }
+    }
+}
